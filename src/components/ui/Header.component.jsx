e96850cae156f030ef2da34bec23a2baa0baab57
Sandeep Gupta <sandeepguptabc@gmail.com>
2021-09-11 11:45:47 +0530

    evaluate on scroll navbar setup

---
+++ src/components/ui/Header.component.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+
+function ElevationScroll(props) {
+	const { children } = props;
+	const trigger = useScrollTrigger({
+		disableHysteresis: true,
+		threshold: 0,
+	});
+
+	return React.cloneElement(children, {
+		elevation: trigger ? 4 : 0,
+	});
+}
+
+export default function ElevateAppBar(props) {
+	return (
+		<React.Fragment>
+			<CssBaseline />
+			<ElevationScroll {...props}>
+				<AppBar>
+					<Toolbar>
+						<Typography variant="h6">Scroll to Elevate App Bar</Typography>
+					</Toolbar>
+				</AppBar>
+			</ElevationScroll>
+			<Toolbar />
+		</React.Fragment>
+	);
+}
