83a8c2dd53e4cd5b63910b34edc9533d70bebf32
Sandeep Gupta <sandeepguptabc@gmail.com>
2021-09-24 10:30:10 +0530

    navigaton through tabs using react router and home page link added to logo click and maintaining active tab state even if page refreshes

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import { ThemeProvider } from "@material-ui/styles";
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "./ui/Header.component";
 
 import theme from "./ui/Theme";
@@ -7,8 +7,32 @@ import theme from "./ui/Theme";
 function App() {
 	return (
 		<ThemeProvider theme={theme}>
-			<Header />
-			Hello
+			<Router>
+				<Header />
+				<Switch>
+					<Route exact path="/" component={() => <div>HOME</div>} />
+					<Route exact path="/services" component={() => <div>Services</div>} />
+					<Route
+						exact
+						path="/customsoftware"
+						component={() => <div>Custom Software</div>}
+					/>
+					<Route
+						exact
+						path="/mobileapps"
+						component={() => <div>Mobile Apps</div>}
+					/>
+					<Route exact path="/websites" component={() => <div>Websites</div>} />
+					<Route
+						exact
+						path="/revolution"
+						component={() => <div>Revolution</div>}
+					/>
+					<Route exact path="/about" component={() => <div>About</div>} />
+					<Route exact path="/contact" component={() => <div>Contact</div>} />
+					<Route exact path="/estimate" component={() => <div>Estimate</div>} />
+				</Switch>
+			</Router>
 		</ThemeProvider>
 	);
 }
--- src/components/ui/Header.component.jsx
+++ src/components/ui/Header.component.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import { Tabs, Tab, Button } from "@material-ui/core";
@@ -26,8 +28,15 @@ const useStyles = makeStyles((theme) => ({
 		marginBottom: "3em",
 	},
 
+	logoContainer: {
+		padding: 0,
+		"&:hover": {
+			backgroundColor: "transparent",
+		},
+	},
+
 	logo: {
-		height: "7em",
+		height: "8em",
 	},
 
 	tabContainer: {
@@ -56,23 +65,77 @@ export default function ElevateAppBar(props) {
 	const handleChange = (e, value) => {
 		setvalue(value);
 	};
+
+	const logoClickHandler = () => {
+		handleChange(null, 0);
+	};
+
+	useEffect(() => {
+		let current_path = window.location.pathname;
+
+		if (current_path === "/" && value !== 0) {
+			setvalue(0);
+		} else if (current_path === "/services" && value !== 1) {
+			setvalue(1);
+		} else if (current_path === "/revolution" && value !== 2) {
+			setvalue(2);
+		} else if (current_path === "/about" && value !== 3) {
+			setvalue(3);
+		} else if (current_path === "/contact" && value !== 4) {
+			setvalue(4);
+		}
+	}, [value]);
+
 	return (
 		<React.Fragment>
 			<ElevationScroll {...props}>
 				<AppBar>
 					<Toolbar disableGutters>
-						<img className={classes.logo} src={logo} alt="company logo" />
+						<Button
+							className={classes.logoContainer}
+							component={Link}
+							to="/"
+							onClick={logoClickHandler}
+							disableRipple
+						>
+							<img className={classes.logo} src={logo} alt="company logo" />
+						</Button>
 						<Tabs
 							value={value}
 							onChange={handleChange}
 							className={classes.tabContainer}
 							indicatorColor="primary"
 						>
-							<Tab className={classes.tab} label="Home" />
-							<Tab className={classes.tab} label="Services" />
-							<Tab className={classes.tab} label="The Revolution" />
-							<Tab className={classes.tab} label="About Us" />
-							<Tab className={classes.tab} label="Contact Us" />
+							<Tab
+								className={classes.tab}
+								component={Link}
+								to="/"
+								label="Home"
+							/>
+							<Tab
+								className={classes.tab}
+								component={Link}
+								to="/services"
+								label="Services"
+							/>
+							<Tab
+								className={classes.tab}
+								component={Link}
+								to="/revolution"
+								label="The Revolution"
+							/>
+							<Tab
+								className={classes.tab}
+								component={Link}
+								to="/about"
+								label="About Us"
+							/>
+							<Tab
+								className={classes.tab}
+								component={Link}
+								to="/contact"
+								label="Contact Us"
+							/>
 						</Tabs>
 						<Button
 							variant="contained"
